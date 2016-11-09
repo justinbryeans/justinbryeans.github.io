5a4699fb3da9a61c613d25eaf74565a271ab5b40
justinbryeans <justinbryeans@gmail.com>
2016-11-08 19:41:37 -0800

    Update Simul in experience section with repo link, comment out Google map functionality in plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -251,7 +251,7 @@ $(document).ready(function() {
 /* Launching Google map */
 
 // When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, "load", init);
+// google.maps.event.addDomListener(window, "load", init);
 
 function init() {
     // Basic options for a simple Google Map
